f70375a0b337d641e327c686330315755089e374
AllAroundD <AllAroundDWeb@gmail.com>
2020-07-02 14:42:48 -0400

    added schema and seeds file and orm router skeleton

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS cards_db;
+
+CREATE DATABASE cards_db;
+
+USE cards_db;
+
+CREATE TABLE decks(
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE cards(
+	`id` INT AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `img` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `deck_id` INT,
+    `main_attribute` VARCHAR(255) NOT NULL,
+    `other_attributes` JSON,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE users(
+	`id` INT AUTO_INCREMENT NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+    `deck_id` INT,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
+);
