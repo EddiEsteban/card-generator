54714ebe61261524467da2e210f8577cd26b865c
AllAroundD <AllAroundDWeb@gmail.com>
2020-07-03 14:26:38 -0400

    modified orm and moved html

--- app/orm.js
+++ app/orm.js
@@ -1,8 +1,62 @@
 const db = require('./connection')
 
-function getList( criteria={} ){
+function getCard( criteria={} ){
     return db.query( 'SELECT * FROM cards '+( criteria ? 'WHERE ? ' : '' ), criteria )
 }
+
+function getName( id ){
+    return db.query( 'SELECT name FROM cards WHERE id=? ', id )
+}
+
+function getImgLocation( id ){
+    return db.query( 'SELECT img FROM cards WHERE id=? ', id )
+}
+
+function getDescription( id ){
+    return db.query( 'SELECT description FROM cards WHERE id=? ', [ id ] )
+}
+
+function setAttributes( id, attributes={} ){
+    // may need to tweak this due to JSON
+    return db.query( 'UPDATE cards SET attributes=? WHERE id=? ', [attributes, id] )
+}
+
+function saveCard( id, name, desc, location, deck_id, attributes ){
+    return db.query( 'UPDATE cards SET ? WHERE id=? ', 
+        [{ name, desc, location, deck_id, attributes }, id ] )
+}
+
+function addDeck( name ){
+    return db.query( 'INSERT INTO decks (name) VALUES (?)', [ name ] )
+}
+
+function deleteCard( id ){
+    return db.query( 'DELETE FROM cards WHERE id=?', [ id ] )
+}
+
+function addCard( name, desc, location, deck_id, attributes){
+    console.log( ' inserting card data: ', { name, desc, location, deck_id, attributes } )
+    return db.query( 'INSERT INTO cards (name, desc, location, deck_id, attributes) VALUES (?,?,?,?,?)', [name, desc, location, deck_id, attributes])
+}
+
+/*
+status
+getName - done
+getImg - done
+getDesc - done
+setAttributes - done (may need to test due to JSON)
+saveCard
+viewCard
+addToDeck
+    createDeck
+
+View Cards
+viewCard/getCard - done
+editCard
+deleteCard - done
+
+deleteAttribute
+
 
 
 
@@ -28,6 +82,8 @@ function deleteTask( id ){
     return db.query( 'DELETE FROM cards WHERE id=?', [ id ] )
 }
 
+*/
+
 module.exports = {
-    getList, insertTask, updateTask, deleteTask
+    getCard, getName, getImgLocation, 
 }
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,12 +2,12 @@ USE cards_db;
 
 INSERT INTO decks (name) VALUES ("Pokemon");
 
-INSERT INTO cards (name, description, img, deck_id, attributes ) VALUES ("Charizard", "Energy Burn: As often as you like during your turn (before your attack), you may turn all Energy attached to Charizard into fire energy for the rest of the turn. This power can't be used if Charizard is Asleep, Confused, or Paralyzed.","https://upload.wikimedia.org/wikipedia/en/1/1f/Pok%C3%A9mon_Charizard_art.png", 1, "Fire Spin: Discard 2 Energy cards attached to Charizard in order to use this attack.");
+INSERT INTO cards (name, description, img, deck_id, attributes ) VALUES ("Charizard", "Energy Burn: As often as you like during your turn (before your attack), you may turn all Energy attached to Charizard into fire energy for the rest of the turn. This power can't be used if Charizard is Asleep, Confused, or Paralyzed.","https://upload.wikimedia.org/wikipedia/en/1/1f/Pok%C3%A9mon_Charizard_art.png", 1, '{"Fire Spin": "Discard 2 Energy cards attached to Charizard in order to use this attack."}');
 
 INSERT INTO users (name) VALUES ("EddiEsteban");
 INSERT INTO users (name) VALUES ("AllAroundD");
+INSERT INTO users (name) VALUES ("SaquibCA");
 
 SELECT * from cards;
 SELECT * from decks;
 SELECT * from users;
-
