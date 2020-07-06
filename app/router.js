f640b6b640b26212d7d2e845489fedf46797c08e
Saquib Uddin Ahmed <saquibuudin@msn.com>
2020-07-06 13:26:16 -0400

    added  router file updates with Fil's help

--- app/router.js
+++ app/router.js
@@ -1,23 +1,23 @@
 const orm = require('./orm');
 
 function router( app ){
-    app.get('/api/cards/:due?', async function(req, res) {
-        const due = req.params.due ? { due: req.params.due } : ''
-        console.log( `[GET] getting list, due=${due}`)
-        const list = await orm.getList( due )
+    app.get('/api/cards/:search?', async function(req, res) {
+        // const search = req.params.search ? { due: req.params.due } : ''
+        console.log( `[GET] getting list, search=${req.params.search}`)
+        const list = await orm.getCard( req.params.search )
 
         res.send( list )
     })
-
+    // function saveCard( id, name, desc, location, deckId, attributes ){
     app.post('/api/cards', async function(req, res) {
         console.log( '[POST] we received this data:', req.body )
-        const saveResult = await orm.insertTask( req.body.priority, req.body.info, req.body.due )
+        const saveResult = await orm.saveCard( req.body )
         console.log( `... insertId: ${saveResult.insertId} ` )
 
         res.send( { status: true, insertId: saveResult.insertId, message: 'Saved successfully' } )
     });
 
-    app.put('/api/cards', async function(req, res) {
+    app.put('/api/tasks', async function(req, res) {
         console.log( '[PUT] we received this data:', req.body )
         if( !req.body.id ) {
             res.status(404).send( { message: 'Invalid id' } )
@@ -28,7 +28,7 @@ function router( app ){
         res.send( { status: true, message: 'Updated successfully' } )
     });
 
-    app.delete('/api/cards/:id', async function(req, res) {
+    app.delete('/api/tasks/:id', async function(req, res) {
         const taskId = req.params.id
         console.log( `[DELETE] id=${taskId}` )
         const deleteResult = await orm.deleteTask( taskId )
