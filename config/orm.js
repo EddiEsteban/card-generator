647d0bce86cef0b4e1ee913a129d882da55e0bcd
AllAroundD <AllAroundDWeb@gmail.com>
2020-07-07 11:00:44 -0400

    updated orm and router

--- config/orm.js
+++ config/orm.js
@@ -1,50 +1,27 @@
 const db = require('./connection')
 
 function getCard( criteria={} ){
-    // return new Promise( (resolve, reject) => {
-    //     db.query( 'SELECT * FROM cards '+ criteria ? 'WHERE ? ' : '', criteria , (err, res) => {
-    //         if (err) {
-    //             reject(new Error(err))
-    //             // throw err
-    //         }
-    //         resolve(res);
-    //     });
-    // })
+    console.log('getting card: ', criteria)
     return db.query( 'SELECT * FROM cards '+( criteria ? 'WHERE ? ' : '' ), criteria )
 }
 
-function getName( id ){
+function getCardName( id ){
     console.log('getting card name')
     return db.query( 'SELECT name FROM cards WHERE id=? ', id )
 }
 
-function getImgLocation( id ){
+function getCardLocation( id ){
     return db.query( 'SELECT img FROM cards WHERE id=? ', id )
 }
 
-function getDescription( id ){
+function getCardDesc( id ){
     return db.query( 'SELECT description FROM cards WHERE id=? ', [ id ] )
 }
 
-function setAttributes( id, attributes={} ){
+function setCardAttributes( id, attributes={} ){
     console.log( ' setting attributes: ', id, attributes )
     // may need to tweak this due to JSON
     return db.query( 'UPDATE cards SET attributes=? WHERE id=? ', [attributes, id] )
-}
-
-function saveCard( id, name, desc, location, deckId, attributes ){
-    console.log( ' saving card: ', id )
-    return db.query( 'UPDATE cards SET ? WHERE id=? ', [{ name, desc, location, deckId, attributes }, id ] )
-}
-
-function addDeck( name ){
-    console.log( ' creating card: ', name )
-    return db.query( 'INSERT INTO decks (name) VALUES (?)', [ name ] )
-}
-
-function deleteCard( id ){
-    console.log( ' deleting card: ', id )
-    return db.query( 'DELETE FROM cards WHERE id=?', [ id ] )
 }
 
 function addCard( name, desc, location, deckId, attributes ){
@@ -54,6 +31,31 @@ function addCard( name, desc, location, deckId, attributes ){
     // console.log(queryString)
     return db.query( queryString)
     // return db.query( `INSERT INTO cards (name, img, desc, deck_id, attributes) VALUES ('${name}','${location}','${desc}',${deckId},'${jsonData}')`)
+}
+
+function saveCard( id, name, desc, location, deckId, attributes ){
+    console.log( ' saving card: ', id )
+    return db.query( 'UPDATE cards SET ? WHERE id=? ', [{ name, desc, location, deckId, attributes }, id ] )
+}
+
+function deleteCard( id ){
+    console.log( ' deleting card: ', id )
+    return db.query( 'DELETE FROM cards WHERE id=?', [ id ] )
+}
+
+function addDeck( name ){
+    console.log( ' creating card: ', name )
+    return db.query( 'INSERT INTO decks (name) VALUES (?)', [ name ] )
+}
+
+function deleteDeck( id ){
+    console.log( ' deleting deck: ', id )
+    return db.query( 'DELETE FROM decks WHERE id=?', [ id ] )
+}
+
+function updateDeck( id, name ){
+    console.log( ' updating deck: ', name )
+    return db.query( 'UPDATE decks SET ? WHERE id=? ', [{ name }, id ] )
 }
 
 /*
@@ -77,5 +79,5 @@ deleteAttribute
 */
 
 module.exports = {
-    getCard, getName, getImgLocation, getDescription, setAttributes, saveCard, addDeck, deleteCard, addCard
+    getCard, getCardName, getCardLocation, getCardDesc, setCardAttributes, saveCard, addDeck, deleteCard, addCard, deleteDeck, updateDeck
 }
