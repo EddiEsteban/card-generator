22f0e90050302321230a3744d733daa0b2a13f8a
AllAroundD <AllAroundDWeb@gmail.com>
2020-07-10 15:16:46 -0400

    added previewCard style and media query

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,7 @@
 
 let attrEnum
 
-function clearCardForm(){
+function clearCardForm() {
     document.querySelector('#cardNameInput').setAttribute('value', '')
     document.querySelector('#cardDescInput').value = ''
     document.querySelector('#cardAttrInputList').innerHTML = ''
@@ -12,12 +12,12 @@ function clearCardForm(){
     document.querySelector('#cardAttrListPreview').innerHTML = ''
 }
 
-function fillCardForm(card){
+function fillCardForm(card) {
     document.querySelector('#cardNameInput').setAttribute('value', card.name)
     document.querySelector('#cardDescInput').value = card.description
     // document.querySelector('#cardImgInput').value = card.img
-    if (card.attributes){
-        card.attributes.forEach((attrval)=>{
+    if (card.attributes) {
+        card.attributes.forEach((attrval) => {
             console.log('fillCard attributes iter', attrval)
             addAttribute(attrval)
             attrEnum++
@@ -30,18 +30,18 @@ function fillCardForm(card){
 }
 
 function previewMatch(id) {
-    let previewId = id.slice(0,-5)+'Preview'
+    let previewId = id.slice(0, -5) + 'Preview'
     let previewEl = document.querySelector(`#${previewId}`)
     let fieldEl = document.querySelector(`#${id}`)
-    if (fieldEl.matches('input')){
+    if (fieldEl.matches('input')) {
         fieldEl.setAttribute('value', fieldEl.value)
-    } else if (fieldEl.matches('textarea')){
+    } else if (fieldEl.matches('textarea')) {
         fieldEl.innerHTML = fieldEl.value
     }
     previewEl.innerHTML = fieldEl.value
 }
 
-function userInputGenerator(attrval={attr: '', val: ''}){
+function userInputGenerator(attrval = { attr: '', val: '' }) {
     console.log('inputGen', attrval)
     let attrHTML = `<label for='attr${attrEnum}Input'>Attribute name</label><input type='text' name='attr${attrEnum}Input' id='attr${attrEnum}Input' class='form-control' onInput='previewMatch(id)' value=${attrval.attr}>`
     let valHTML = `<label for='val${attrEnum}Input'>Value</label><textarea name='val${attrEnum}Input' id='val${attrEnum}Input' class='form-control' onInput='previewMatch(id)'>${attrval.val}</textarea>`
@@ -49,96 +49,96 @@ function userInputGenerator(attrval={attr: '', val: ''}){
 }
 
 
-function addAttribute(attrval={attr: '', val: ''}){
+function addAttribute(attrval = { attr: '', val: '' }) {
     let attrListEl = document.querySelector('#cardAttrInputList')
     let previewAttrEl = document.querySelector('#cardAttrListPreview')
-    console.log({attrval, attrListEl, previewAttrEl})
+    console.log({ attrval, attrListEl, previewAttrEl })
     attrListEl.innerHTML += userInputGenerator(attrval)
     previewAttrEl.innerHTML += `<li class='list-group-item'><div class='row'><div class='col' id='attr${attrEnum}Preview'>${attrval.attr}</div><div class='col' id='val${attrEnum}Preview'>${attrval.val}</div></div></li>`
-    attrEnum +=1
+    attrEnum += 1
 }
 
 // api requests below
 
-async function apiCall( url, method='get', data={} ){
+async function apiCall(url, method = 'get', data = {}) {
     method = method.toLowerCase()
     let settings = { method }
 
     // for formData we must NOT set content-type, let system do it
-    const isFormData = (typeof data)==='string'
-    if( !isFormData ) {
+    const isFormData = (typeof data) === 'string'
+    if (!isFormData) {
         settings.headers = { 'Content-Type': 'application/json' }
     }
 
     // only attach the body for put/post
-    if( method === 'post' || method === 'put' ) {
-        if( isFormData ){
+    if (method === 'post' || method === 'put') {
+        if (isFormData) {
             //* gather form data (esp. if attached media)
             //! each entry to be attached must have a valid **name** attribute
-            settings.body = new FormData( document.querySelector(`${data}`) )
+            settings.body = new FormData(document.querySelector(`${data}`))
         } else {
-            settings.body = JSON.stringify( data )
+            settings.body = JSON.stringify(data)
         }
     }
 
-    const result = await fetch( url,settings ).then( res=>res.json() )
+    const result = await fetch(url, settings).then(res => res.json())
 
     return result
 }
 
-function showCardForm(event){
+function showCardForm(event) {
     attrEnum = 0
     event.preventDefault()
     clearCardForm()
     let crudButton = document.querySelector('#crudButtons')
     document.querySelector('#imageFile').value = null
-    if (event.target.id == 'createCardInit'){
+    if (event.target.id == 'createCardInit') {
         document.querySelector('#mediaForm').setAttribute('method', 'POST')
         crudButton.innerHTML = `<button type='submit' class='btn btn-primary' onClick='createCard(event)'>Create card</button>`
-    } else if (event.target.classList.contains('editBtn')){
+    } else if (event.target.classList.contains('editBtn')) {
         document.querySelector('#mediaForm').setAttribute('method', 'PUT')
-        
+
         crudButton.innerHTML = `<button type='submit' class='btn btn-primary' onClick='editCard(event)'>Edit card</button>`
     }
     let cardFormEl = document.querySelector('#cardFormBlock')
     cardFormEl.classList.remove('d-none')
 }
 
-function hideCardForm(){
+function hideCardForm() {
     let cardFormEl = document.querySelector('#cardFormBlock')
     cardFormEl.classList.add('d-none')
 }
 
-let cardThumbnail = (card)=>{
-    return `<div class="card col-4 col-sm-3 col-md-2" data-card-id='${card.id}' data-card-img-url='${card.img}'>`+
-        `<img src="${card.img}" class="card-img-top img-fluid" alt="...">`+
-        `<div class="card-body">`+
-        `<h6 class="card-title">${card.name}</h6>`+
-        `<button class='btn btn-secondary editBtn' onClick='getCard(event)'>📝</button>`+
-        `<button class='btn btn-danger delBtn' onClick='deleteCard(event)'>🗑</button>`+
+let cardThumbnail = (card) => {
+    return `<div class="card col-4 col-sm-3 col-md-2" data-card-id='${card.id}' data-card-img-url='${card.img}'>` +
+        `<img src="${card.img}" class="card-img-top img-fluid" alt="...">` +
+        `<div class="card-body cardPreviewBody">` +
+        `<h6 class="card-title previewCard">${card.name}</h6>` +
+        `<button class='btn btn-secondary editBtn' onClick='getCard(event)'>📝</button>` +
+        `<button class='btn btn-danger delBtn' onClick='deleteCard(event)'>🗑</button>` +
         `</div></div>`
 }
 
-async function showAllCards(){
+async function showAllCards() {
     let cards = await apiCall('/api/cards')
     let cardListEl = document.querySelector('#cardListBlock')
     cardListEl.innerHTML = ''
-    cards.forEach(card=>{
+    cards.forEach(card => {
         cardListEl.innerHTML = cardThumbnail(card) + cardListEl.innerHTML
     })
 }
 
-async function showDeckCards(deckId){
-    let cards = await apiCall(`/api/cards/deck/${deckId}` )
+async function showDeckCards(deckId) {
+    let cards = await apiCall(`/api/cards/deck/${deckId}`)
     let cardListEl = document.querySelector('#cardListBlock')
     document.querySelector('#cardListMain').classList.remove('d-none')
     cardListEl.innerHTML = ''
-    cards.forEach(card=>{
+    cards.forEach(card => {
         cardListEl.innerHTML += cardThumbnail(card)
     })
 }
 
-async function getCard(event){
+async function getCard(event) {
     event.preventDefault()
     let cardEl = event.target.parentNode.parentNode
     let id = cardEl.dataset.cardId
@@ -151,7 +151,7 @@ async function getCard(event){
     fillCardForm(card)
 }
 
-async function editCard(event){
+async function editCard(event) {
     event.preventDefault()
     let result = await apiCall('/api/cards', 'put', '#mediaForm')
     clearCardForm()
@@ -160,7 +160,7 @@ async function editCard(event){
     return result
 }
 
-async function createCard(event){
+async function createCard(event) {
     event.preventDefault()
     // let nameInputEl = document.querySelector('#cardNameInput')
     // let imgInputEl = document.querySelector('#cardImgInput')
@@ -192,7 +192,7 @@ async function createCard(event){
     return result
 }
 
-async function deleteCard(event){
+async function deleteCard(event) {
     event.preventDefault()
     let cardEl = event.target.parentNode.parentNode
     let id = cardEl.dataset.cardId
@@ -201,17 +201,17 @@ async function deleteCard(event){
     return result
 }
 
-function fillDeckForm(deck){
+function fillDeckForm(deck) {
     document.querySelector('#deckId').value = deck.id
     document.querySelector('#deckNameInput').value = deck.name
 }
 
-function clearDeckForm(){
+function clearDeckForm() {
     document.querySelector('#deckId').value = ''
     document.querySelector('#deckNameInput').value = ''
 }
 
-function showDeckForm(event, id){
+function showDeckForm(event, id) {
     event.preventDefault()
     clearDeckForm()
     let cardFormEl = document.querySelector('#cardListMain')
@@ -228,26 +228,26 @@ function showDeckForm(event, id){
 
 
 
-let deckThumbnail = (deck)=>{
-    return `<div class="card col-4 col-sm-3 col-md-2" data-deck-id='${deck.id}'>`+
-        `<img src="${deck.img}" class="card-img-top img-fluid" alt="...">`+
-        `<div class="card-body">`+
-        `<h6 class="card-title">${deck.name}</h6>`+
-        `<button class='btn btn-secondary editBtn' onClick='getDeck(event)'>📝</button>`+
-        `<button class='btn btn-danger delBtn' onClick='deleteDeck(event)'>🗑</button>`+
+let deckThumbnail = (deck) => {
+    return `<div class="card col-4 col-sm-3 col-md-2" data-deck-id='${deck.id}'>` +
+        `<img src="${deck.img}" class="card-img-top img-fluid" alt="...">` +
+        `<div class="card-body">` +
+        `<h6 class="card-title">${deck.name}</h6>` +
+        `<button class='btn btn-secondary editBtn' onClick='getDeck(event)'>📝</button>` +
+        `<button class='btn btn-danger delBtn' onClick='deleteDeck(event)'>🗑</button>` +
         `</div></div>`
 }
 
-async function showAllDecks(){
+async function showAllDecks() {
     let decks = await apiCall('/api/decks')
     let deckListEl = document.querySelector('#deckListBlock')
     deckListEl.innerHTML = ''
-    decks.forEach(deck=>{
+    decks.forEach(deck => {
         deckListEl.innerHTML = deckThumbnail(deck) + deckListEl.innerHTML
     })
 }
 
-async function getDeck(event){
+async function getDeck(event) {
     event.preventDefault()
     console.log(event.target.parentNode.parentNode)
     let deckEl = event.target.parentNode.parentNode
@@ -258,7 +258,7 @@ async function getDeck(event){
     fillDeckForm(deck)
 }
 
-async function editDeck(event){
+async function editDeck(event) {
     event.preventDefault()
     let deckEl = event.target.parentNode.parentNode
     console.log('deckEl: ', deckEl)
@@ -277,7 +277,7 @@ async function editDeck(event){
     return response
 }
 
-async function deleteDeck(){
+async function deleteDeck() {
     event.preventDefault()
     let deckEl = event.target.parentNode.parentNode
     let id = deckEl.dataset.deckId
@@ -287,23 +287,23 @@ async function deleteDeck(){
     return result
 }
 
-function previewImg(event){
+function previewImg(event) {
     let output = document.getElementById('cardImgPreview');
     output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
+    output.onload = function () {
         URL.revokeObjectURL(output.src) // free memory
     }
 }
 
-async function mediaList( id ){
+async function mediaList(id) {
     // const getRequest = await apiCall( '/api/media' )
     // console.log( '[mediaList] ', getRequest )
 
-    if( getRequest.status ){
-        const mediaListEl = document.querySelector( '#mediaList' )
+    if (getRequest.status) {
+        const mediaListEl = document.querySelector('#mediaList')
         mediaListEl.innerHTML = ''
 
-        getRequest.mediaList.forEach( function( mediaUrl ){
+        getRequest.mediaList.forEach(function (mediaUrl) {
             mediaListEl.innerHTML += `
             <img src='${mediaUrl}' class='img-thumbnail' width=100>
             `
@@ -313,15 +313,15 @@ async function mediaList( id ){
 }
 
 // save the new form
-async function uploadMedia( event ){
+async function uploadMedia(event) {
     event.preventDefault()
 
     //* because we are using the built-in browser form-builder, we need valid
     //! **name** attributes - for ease we give same values as the id's
-    const uploadResponse = await apiCall( '/api/media', 'post', '#mediaForm' )
+    const uploadResponse = await apiCall('/api/media', 'post', '#mediaForm')
     // console.log( '[uploadResponse] ', uploadResponse )
 
-    if( uploadResponse.status ){
+    if (uploadResponse.status) {
         // clear the data
         document.querySelector('#imageUrl').value = ''
         document.querySelector('#imageFile').value = ''
@@ -331,7 +331,7 @@ async function uploadMedia( event ){
     }
 }
 
-async function mainApp(){
+async function mainApp() {
     await showAllDecks()
 
     await showAllCards()
